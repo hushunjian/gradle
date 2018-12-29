0fce2d8af9b96d2d60369652819887b65593b196
hushunjian <hushunjian950420@163.com>
2018-12-29 16:22:18 +0800

    fixbug

--- src/main/java/com/hushunjian/gradle/service/QueryTestService.java
+++ src/main/java/com/hushunjian/gradle/service/QueryTestService.java
@@ -20,6 +20,7 @@ import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Service;
 
 import com.hushunjian.gradle.entity.ImportantTaskV2Entity;
@@ -28,74 +29,143 @@ import com.hushunjian.gradle.repo.ImportantTaskV2Repo;
 @Service
 @Transactional
 public class QueryTestService {
-	
+
 	private final static String id = "id";
-	
+
 	private final static String importantTaskName = "importantTaskName";
-	
+
 	private final static String startDate = "startDate";
-	
+
 	private final static String groupId = "group_id";
+
+	private final static String taskName = "taskV2_name";
+
+	private final static String taskId = "taskV2_id";
 
 	@Autowired
 	private ImportantTaskV2Repo importantTaskV2Repo;
 
 	public void test() {
+		/**
+		 * 传入的查询条件
+		 */
 		Map<String, Map<String, Object>> criteria = new HashMap<>();
-/*		Map<String,Object> map1 = new HashMap<>();
-		map1.put("notEq", 2);
-		criteria.put(id, map1);*/
-		Map<String,Object> map2 = new HashMap<>();
-		map2.put("eq", "构件组1");
-		criteria.put(importantTaskName, map2);
-/*		Map<String,Object> map3 = new HashMap<>();
-		map3.put("le", ZonedDateTime.now());
-		criteria.put(startDate, map3);*/
-		Map<String,Object> map4 = new HashMap<>();
-		map4.put("eq", 1);
-		criteria.put(groupId, map4);
-
+		criteria.put(id, conditionValueToMap("notEq", 1));
+		criteria.put(importantTaskName, conditionValueToMap("eq", "构件组1"));
+		criteria.put(startDate, conditionValueToMap("le", ZonedDateTime.now()));
+		criteria.put(groupId, conditionValueToMap("eq", 1));
+		criteria.put(taskId, conditionValueToMap("eq", 1));
+		criteria.put(taskName, conditionValueToMap("eq", "构件组1"));
+		/**
+		 * 筛选符合实体的查询条件
+		 */
 		final Map<String, Map<String, Object>> result = new HashMap<>();
-		result.putAll(a(criteria, ImportantTaskV2Entity.class));
+		result.putAll(filter(criteria, ImportantTaskV2Entity.class));
+		
+		/**
+		 * 动态查询
+		 */
 		List<ImportantTaskV2Entity> findAll = importantTaskV2Repo.findAll((root, query, cb) -> {
 			Predicate predicate = cb.conjunction();
 			List<Expression<Boolean>> expressions = predicate.getExpressions();
-			expressions.addAll(b(result,cb,root));
+			expressions.addAll(appendCriteria(result, cb, root));
 			return predicate;
 		});
-		findAll.forEach(importantTask ->{
+		
+		/**
+		 * 输出结果
+		 */
+		findAll.forEach(importantTask -> {
 			System.out.println("==========");
-			System.out.println("id:"+importantTask.getId());
-			System.out.println("importantTaskName:"+importantTask.getImportantTaskName());
-			System.out.println("startDate:"+importantTask.getStartDate());
+			System.out.println("id:" + importantTask.getId());
+			System.out.println("importantTaskName:" + importantTask.getImportantTaskName());
+			System.out.println("startDate:" + importantTask.getStartDate());
+			System.out.println("==========");
+		});
+		
+		List<ImportantTaskV2Entity> findAll1 = test(importantTaskV2Repo,result);
+		/**
+		 * 输出结果
+		 */
+		findAll1.forEach(importantTask -> {
+			System.out.println("==========");
+			System.out.println("id:" + importantTask.getId());
+			System.out.println("importantTaskName:" + importantTask.getImportantTaskName());
+			System.out.println("startDate:" + importantTask.getStartDate());
 			System.out.println("==========");
 		});
 	}
+	
+	/**
+	 * 封装查询
+	 * 
+	 * @param repo
+	 * @param result
+	 * @return
+	 */
+	private <T extends JpaSpecificationExecutor<?>> List test(T repo,Map<String, Map<String, Object>> result){
+		  return repo.findAll((root, query, cb) -> {
+				Predicate predicate = cb.conjunction();
+				List<Expression<Boolean>> expressions = predicate.getExpressions();
+				expressions.addAll(appendCriteria(result, cb, root));
+				return predicate;
+			});
+		}
 
-	private List<Expression<Boolean>> b(Map<String, Map<String, Object>> criterias,CriteriaBuilder cb, Root<?> root) {
+	/**
+	 * 将查询条件和值组装成map
+	 * @param condition
+	 * @param value
+	 * @return
+	 */
+	private Map<String, Object> conditionValueToMap(String condition, Object value) {
+		Map<String, Object> map = new HashMap<>();
+		map.put(condition, value);
+		return map;
+	}
+
+	/**
+	 * 拼接条件
+	 * 
+	 * @param criterias
+	 * @param cb
+	 * @param root
+	 * @return
+	 */
+	private List<Expression<Boolean>> appendCriteria(Map<String, Map<String, Object>> criterias, CriteriaBuilder cb, Root<?> root) {
 		List<Expression<Boolean>> expressions = new ArrayList<>();
 		criterias.forEach((propertyName, values) -> {
 			values.forEach((criteria, value) -> {
 				if (value != null) {
-					expressions.add(c(propertyName,criteria,value,cb,root));
+					expressions.add(appendCriteriaValue(propertyName, criteria, value, cb, root));
 				}
 			});
 		});
 		return expressions;
 	}
 
-	private Predicate c(String propertyName, String criteria, Object value, CriteriaBuilder cb, Root<?> root) {
+	/**
+	 * 针对每一个属性拼接条件
+	 * 
+	 * @param propertyName
+	 * @param criteria
+	 * @param value
+	 * @param cb
+	 * @param root
+	 * @return
+	 */
+	private Predicate appendCriteriaValue(String propertyName, String criteria, Object value, CriteriaBuilder cb, Root<?> root) {
 		Predicate predicate = null;
 		switch (criteria) {
 		case "eq":
 			if (propertyName.contains("_")) {
 				String[] split = propertyName.split("_");
 				Path<Object> path = root.get(split[0]);
-				for(int i = 1;i<split.length;i++){
+				for (int i = 1; i < split.length; i++) {
 					path = path.get(split[i]);
 				}
 				predicate = cb.equal(path, value);
-			}else{
+			} else {
 				predicate = cb.equal(root.get(propertyName), value);
 			}
 			break;
@@ -107,13 +177,13 @@ public class QueryTestService {
 			break;
 		case "le":
 			if (value instanceof Number) {
-				predicate = cb.le(root.get(propertyName), (Number)value);
-			}else if(value instanceof Date){
-				predicate = cb.lessThanOrEqualTo(root.get(propertyName), (Date)value);
-			}else if(value instanceof ZonedDateTime){
-				predicate = cb.lessThanOrEqualTo(root.get(propertyName), (ZonedDateTime)value);
-			}else if(value instanceof LocalDate){
-				predicate = cb.lessThanOrEqualTo(root.get(propertyName), (LocalDate)value);
+				predicate = cb.le(root.get(propertyName), (Number) value);
+			} else if (value instanceof Date) {
+				predicate = cb.lessThanOrEqualTo(root.get(propertyName), (Date) value);
+			} else if (value instanceof ZonedDateTime) {
+				predicate = cb.lessThanOrEqualTo(root.get(propertyName), (ZonedDateTime) value);
+			} else if (value instanceof LocalDate) {
+				predicate = cb.lessThanOrEqualTo(root.get(propertyName), (LocalDate) value);
 			}
 		default:
 			break;
@@ -128,7 +198,7 @@ public class QueryTestService {
 	 * @param clazz
 	 * @return
 	 */
-	private Map<String, Map<String, Object>> a(Map<String, Map<String, Object>> criteria, Class<?> clazz) {
+	private Map<String, Map<String, Object>> filter(Map<String, Map<String, Object>> criteria, Class<?> clazz) {
 		Field[] fields = clazz.getDeclaredFields();
 		List<String> fieldNames = new ArrayList<>();
 		for (Field field : fields) {
@@ -142,11 +212,11 @@ public class QueryTestService {
 			if (next.getKey().contains("_")) {
 				// 外键
 				try {
-					d(next.getKey(),clazz);
+					d(next.getKey(), clazz);
 				} catch (Exception e) {
 					iterator.remove();
 				}
-			}else{
+			} else {
 				if (!fieldNames.contains(next.getKey())) {
 					iterator.remove();
 				}
@@ -154,15 +224,24 @@ public class QueryTestService {
 		}
 		return criteria;
 	}
-	
-	private void d(String foreignKey, Class<?> clazz) throws NoSuchFieldException{
+
+	/**
+	 * 针对外键进行判断
+	 * 
+	 * @param foreignKey
+	 * @param clazz
+	 * @throws NoSuchFieldException
+	 */
+	private void d(String foreignKey, Class<?> clazz) throws NoSuchFieldException {
 		if (!foreignKey.contains("_")) {
+			// 已经不包含下划线了说明是最后的条件属性,判断实体中是否存在该字段
+			clazz.getDeclaredField(foreignKey);
 			return;
 		}
 		String substring = foreignKey.substring(0, foreignKey.indexOf("_"));
 		Field field = clazz.getDeclaredField(substring);
 		Class<?> type = field.getType();
-		foreignKey = foreignKey.substring(foreignKey.indexOf("_")+1,foreignKey.length());
-		d(foreignKey,type);
+		foreignKey = foreignKey.substring(foreignKey.indexOf("_") + 1, foreignKey.length());
+		d(foreignKey, type);
 	}
 }
